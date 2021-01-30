eca3e241d3c9b7f1324c90a46f55c2574b562796
Kasun Vithanage <alankasun@gmail.com>
2021-01-30 17:26:35 +0530

    add nanoid

--- server/mix.exs
+++ server/mix.exs
@@ -21,7 +21,8 @@ defmodule TicTacToe.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.23", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
+      {:nanoid, "~> 2.0.5"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
