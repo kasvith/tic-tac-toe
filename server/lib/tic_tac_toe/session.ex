3c76cafb33a285fb7e10611b62d59f61490c99be
Kasun Vithanage <alankasun@gmail.com>
2021-02-01 00:19:50 +0530

    complete session

--- server/lib/tic_tac_toe/session.ex
+++ server/lib/tic_tac_toe/session.ex
@@ -1,6 +1,6 @@
 defmodule TicTacToe.Session do
   require Logger
-  use GenServer, restart: :temporary
+  use GenServer, restart: :transient
 
   defstruct [:session_id, game: TicTacToe.Game.new(), players: %{}]
 
@@ -8,13 +8,14 @@ defmodule TicTacToe.Session do
   @timeout 600_000
 
   # client
+  @spec start_link(String.t()) :: :ignore | {:error, any} | {:ok, pid}
   def start_link(session_id) do
     GenServer.start_link(__MODULE__, session_id)
   end
 
   @spec move(atom | pid | {atom, any} | {:via, atom, any}, String.t(), integer()) :: any
-  def move(session_pid, player, position) do
-    GenServer.call(session_pid, {:move, player, position}, @timeout)
+  def move(session_pid, player_id, position) do
+    GenServer.call(session_pid, {:move, player_id, position}, @timeout)
   end
 
   @spec get_status(atom | pid | {atom, any} | {:via, atom, any}) :: any
@@ -27,8 +28,14 @@ defmodule TicTacToe.Session do
     GenServer.call(session_pid, :get_game)
   end
 
+  @spec join_game(atom | pid | {atom, any} | {:via, atom, any}, any) :: any
   def join_game(session_pid, player_id) do
     GenServer.call(session_pid, {:join_game, player_id})
+  end
+
+  @spec leave_game(atom | pid | {atom, any} | {:via, atom, any}, any) :: any
+  def leave_game(session_pid, player_id) do
+    GenServer.cast(session_pid, {:leave_game, player_id})
   end
 
   # server
@@ -38,7 +45,13 @@ defmodule TicTacToe.Session do
   end
 
   @impl GenServer
-  def handle_call({:move, player, position}, _from, %{game: game} = state) do
+  def handle_call(
+        {:move, player_id, position},
+        _from,
+        %TicTacToe.Session{game: game, players: players} = state
+      ) do
+    player = Map.get(players, player_id)
+
     case TicTacToe.Game.move(game, player, position) do
       {:ok, new_game} ->
         {
@@ -59,7 +72,7 @@ defmodule TicTacToe.Session do
   end
 
   @impl GenServer
-  def handle_call(:get_status, _from, %{game: game} = state) do
+  def handle_call(:get_status, _from, %TicTacToe.Session{game: game} = state) do
     {
       :reply,
       {
@@ -76,7 +89,7 @@ defmodule TicTacToe.Session do
   end
 
   @impl GenServer
-  def handle_call(:get_game, _from, %{game: game} = state) do
+  def handle_call(:get_game, _from, %TicTacToe.Session{game: game} = state) do
     {
       :reply,
       game,
@@ -86,17 +99,62 @@ defmodule TicTacToe.Session do
   end
 
   @impl GenServer
-  def handle_call(:join_game, _from, %{game: game} = state) do
+  def handle_call({:join_game, _player_id}, _from, %TicTacToe.Session{players: players} = state)
+      when is_map(players) and map_size(players) >= 2 do
     {
       :reply,
-      game,
+      {:error, "room full"},
       state,
       @timeout
     }
   end
 
   @impl GenServer
-  def handle_info(:timeout, %{session_id: session_id} = state) do
+  def handle_call({:join_game, player_id}, _from, %TicTacToe.Session{players: players} = state) do
+    if registered?(players, player_id) do
+      {
+        :reply,
+        {:ok, Map.get(players, player_id)},
+        state,
+        @timeout
+      }
+    else
+      sign = get_player_sign(players)
+      new_players = Map.put(players, player_id, sign)
+
+      {
+        :reply,
+        {:ok, sign},
+        %TicTacToe.Session{state | players: new_players},
+        @timeout
+      }
+    end
+  end
+
+  defp registered?(players, player_id) do
+    Map.has_key?(players, player_id)
+  end
+
+  defp get_player_sign(players) do
+    has_x = players |> Map.values() |> Enum.member?("X")
+
+    case has_x do
+      true -> "O"
+      false -> "X"
+    end
+  end
+
+  @impl GenServer
+  def handle_cast({:leave_game, player_id}, %TicTacToe.Session{players: players} = state) do
+    {
+      :noreply,
+      %TicTacToe.Session{state | players: Map.delete(players, player_id)},
+      @timeout
+    }
+  end
+
+  @impl GenServer
+  def handle_info(:timeout, %TicTacToe.Session{session_id: session_id} = state) do
     Logger.info("Ending game session #{session_id} due to inactivity")
     {:stop, :normal, state}
   end
--- server/test/tic_tac_toe/session_test.exs
+++ server/test/tic_tac_toe/session_test.exs
@@ -1,26 +1,64 @@
 defmodule TicTacToe.Session.Test do
   use ExUnit.Case
 
-  test "allow valid moves" do
+  test "allow joining a session" do
+    {:ok, pid} = TicTacToe.Session.start_link("game1")
+    assert {:ok, "X"} = TicTacToe.Session.join_game(pid, "1")
+    assert {:ok, "O"} = TicTacToe.Session.join_game(pid, "2")
+  end
+
+  test "dont allow joining when room is full" do
+    {:ok, pid} = TicTacToe.Session.start_link("game1")
+    assert {:ok, "X"} = TicTacToe.Session.join_game(pid, "1")
+    assert {:ok, "O"} = TicTacToe.Session.join_game(pid, "2")
+    assert {:error, "room full"} = TicTacToe.Session.join_game(pid, "1000")
+  end
+
+  test "allow leaving a session" do
     {:ok, pid} = TicTacToe.Session.start_link("game1")
 
-    assert :ok = TicTacToe.Session.move(pid, "X", 0)
-    assert :ok = TicTacToe.Session.move(pid, "O", 1)
-    assert :ok = TicTacToe.Session.move(pid, "X", 8)
+    assert {:ok, "X"} = TicTacToe.Session.join_game(pid, "1")
+    assert {:ok, "O"} = TicTacToe.Session.join_game(pid, "2")
+
+    # leave 1
+    TicTacToe.Session.leave_game(pid, "1")
+
+    # new user id can now login as X
+    assert {:ok, "X"} = TicTacToe.Session.join_game(pid, "100")
+  end
+
+  test "allow valid moves" do
+    {:ok, pid} = TicTacToe.Session.start_link("game1")
+    TicTacToe.Session.join_game(pid, "1")
+    TicTacToe.Session.join_game(pid, "2")
+
+    assert :ok = TicTacToe.Session.move(pid, "1", 0)
+    assert :ok = TicTacToe.Session.move(pid, "2", 1)
+    assert :ok = TicTacToe.Session.move(pid, "1", 8)
   end
 
   test "disallow invalid moves" do
     {:ok, pid} = TicTacToe.Session.start_link("game1")
+    TicTacToe.Session.join_game(pid, "1")
+    TicTacToe.Session.join_game(pid, "2")
 
-    assert :ok = TicTacToe.Session.move(pid, "X", 0)
-    assert {:error, _reason} = TicTacToe.Session.move(pid, "O", 0)
+    assert :ok = TicTacToe.Session.move(pid, "1", 0)
+    assert {:error, _reason} = TicTacToe.Session.move(pid, "2", 0)
   end
 
   test "gives correct status after a move" do
     {:ok, pid} = TicTacToe.Session.start_link("game1")
+    TicTacToe.Session.join_game(pid, "1")
+    TicTacToe.Session.join_game(pid, "2")
 
-    assert :ok = TicTacToe.Session.move(pid, "X", 0)
+    assert :ok = TicTacToe.Session.move(pid, "1", 0)
 
     assert {:winner, nil, :board_full, false, :player, "O"} = TicTacToe.Session.get_status(pid)
+  end
+
+  test "gets game" do
+    {:ok, pid} = TicTacToe.Session.start_link("game1")
+
+    assert TicTacToe.Game.new() == TicTacToe.Session.get_game(pid)
   end
 end
