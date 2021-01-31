187d9eceec96c2680d41ece80f204ca61ed1f39a
Kasun Vithanage <alankasun@gmail.com>
2021-01-31 13:28:14 +0530

    modify session

--- server/lib/tic_tac_toe/session.ex
+++ server/lib/tic_tac_toe/session.ex
@@ -1,48 +1,59 @@
 defmodule TicTacToe.Session do
-  use GenServer
+  require Logger
+  use GenServer, restart: :temporary
+
+  # 10 mins
+  @timeout 600_000
 
   # client
-  def start_link() do
-    GenServer.start_link(__MODULE__, nil)
+  def start_link(session_id) do
+    GenServer.start_link(__MODULE__, session_id)
   end
 
   @spec move(atom | pid | {atom, any} | {:via, atom, any}, String.t(), integer()) :: any
   def move(session_pid, player, position) do
-    GenServer.call(session_pid, {:move, player, position})
+    GenServer.call(session_pid, {:move, player, position}, @timeout)
   end
 
   @spec get_status(atom | pid | {atom, any} | {:via, atom, any}) :: any
   def get_status(session_pid) do
-    GenServer.call(session_pid, :get_status)
+    GenServer.call(session_pid, :get_status, @timeout)
+  end
+
+  @spec get_game(atom | pid | {atom, any} | {:via, atom, any}) :: any
+  def get_game(session_pid) do
+    GenServer.call(session_pid, :get_game)
   end
 
   # server
   @impl GenServer
-  def init(_) do
-    {:ok, TicTacToe.Game.new()}
+  def init(session_id) do
+    {:ok, {session_id, TicTacToe.Game.new()}, @timeout}
   end
 
   @impl GenServer
-  def handle_call({:move, player, position}, _from, game) do
+  def handle_call({:move, player, position}, _from, {session_id, game}) do
     case TicTacToe.Game.move(game, player, position) do
       {:ok, new_game} ->
         {
           :reply,
           :ok,
-          new_game
+          {session_id, new_game},
+          @timeout
         }
 
       {:error, reason} ->
         {
           :reply,
           {:error, reason},
-          game
+          {session_id, game},
+          @timeout
         }
     end
   end
 
   @impl GenServer
-  def handle_call(:get_status, _from, game) do
+  def handle_call(:get_status, _from, {session_id, game}) do
     {
       :reply,
       {
@@ -53,7 +64,24 @@ defmodule TicTacToe.Session do
         :player,
         game.current_player
       },
-      game
+      {session_id, game},
+      @timeout
     }
+  end
+
+  @impl GenServer
+  def handle_call(:get_game, _from, {session_id, game}) do
+    {
+      :reply,
+      game,
+      {session_id, game},
+      @timeout
+    }
+  end
+
+  @impl GenServer
+  def handle_info(:timeout, {session_id, game}) do
+    Logger.info("Ending game session #{session_id} due to inactivity")
+    {:stop, :normal, {session_id, game}}
   end
 end
--- server/test/tic_tac_toe/session_test.exs
+++ server/test/tic_tac_toe/session_test.exs
@@ -2,7 +2,7 @@ defmodule TicTacToe.Session.Test do
   use ExUnit.Case
 
   test "allow valid moves" do
-    {:ok, pid} = TicTacToe.Session.start_link()
+    {:ok, pid} = TicTacToe.Session.start_link("game1")
 
     assert :ok = TicTacToe.Session.move(pid, "X", 0)
     assert :ok = TicTacToe.Session.move(pid, "O", 1)
@@ -10,14 +10,14 @@ defmodule TicTacToe.Session.Test do
   end
 
   test "disallow invalid moves" do
-    {:ok, pid} = TicTacToe.Session.start_link()
+    {:ok, pid} = TicTacToe.Session.start_link("game1")
 
     assert :ok = TicTacToe.Session.move(pid, "X", 0)
     assert {:error, _reason} = TicTacToe.Session.move(pid, "O", 0)
   end
 
   test "gives correct status after a move" do
-    {:ok, pid} = TicTacToe.Session.start_link()
+    {:ok, pid} = TicTacToe.Session.start_link("game1")
 
     assert :ok = TicTacToe.Session.move(pid, "X", 0)
 
