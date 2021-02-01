51b942e2a0fc5cc57d0b0da38ee91d03d4f66898
Kasun Vithanage <alankasun@gmail.com>
2021-02-01 09:50:56 +0530

    mix fmt

--- server/lib/tic_tac_toe/lobby.ex
+++ server/lib/tic_tac_toe/lobby.ex
@@ -5,8 +5,8 @@ defmodule TicTacToe.Lobby do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
-  def create_session() do
-    GenServer.call(__MODULE__, :create_session)
+  def create_session(name) do
+    GenServer.call(__MODULE__, {:create_session, name})
   end
 
   def join_session(session_id, player_id) do
@@ -19,15 +19,18 @@ defmodule TicTacToe.Lobby do
   end
 
   @impl GenServer
-  def handle_call(:create_session, _from, store) do
-    id = create_game_session_id(store)
+  def handle_call({:create_session, name}, _from, store) do
+    # id = create_game_session_id(store, name)
   end
 
-  defp create_game_session_id(store) do
+  defp create_session_id do
+  end
+
+  defp generate_game_session_id(store) do
     id = Nanoid.generate(10)
 
     case Map.has_key?(store, id) do
-      true -> create_game_session_id(store)
+      true -> generate_game_session_id(store)
       false -> id
     end
   end
