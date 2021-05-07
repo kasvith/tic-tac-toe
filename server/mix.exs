dac51ac8e931cfb738e9726b55846854fe6a0d2b
Kasun Vithanage <alankasun@gmail.com>
2021-05-07 17:11:16 +0530

    gproc

--- server/mix.exs
+++ server/mix.exs
@@ -22,7 +22,8 @@ defmodule TicTacToe.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
-      {:nanoid, "~> 2.0.5"}
+      {:nanoid, "~> 2.0.5"},
+      {:gproc, "~> 0.9.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
