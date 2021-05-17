cf7de5139f98b2fc0406c8891e0672bf8b642b16
Kasun Vithanage <alankasun@gmail.com>
2021-05-18 00:59:19 +0530

    some changes

--- server/lib/tic_tac_toe/lobby.ex
+++ server/lib/tic_tac_toe/lobby.ex
@@ -1,51 +1,17 @@
 defmodule TicTacToe.Lobby do
-  use GenServer
-
-  def start_link() do
-    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
-  end
-
-  def create_session() do
-    GenServer.call(__MODULE__, {:create_session})
-  end
-
-  def join_session(session_id, player_id) do
-    GenServer.call(__MODULE__, {:join_session, session_id, player_id})
-  end
-
-  @impl GenServer
-  def init(_arg) do
-    {:ok, %{}}
-  end
-
-  @impl GenServer
-  def handle_call({:create_session}, _from, store) do
-    reply = generate_game_session_id(store)
-
-    {
-      :reply,
-      reply,
-      store
-    }
-  end
-
-  defp generate_game_session_id(store) do
+  def generate_game_session_id() do
     id = Nanoid.generate(10)
 
-    case Map.has_key?(store, id) do
+    case is_game_started?(id) do
       false -> {:ok, id}
-      true -> generate_game_session_id(store)
+      true -> generate_game_session_id()
     end
   end
 
-  # @impl GenServer
-  # def handle_call({:join_session, session_id, player_id}, _from, store) do
-  # end
-
-  defp get_game(store, session_id) do
-    case Map.get(store, session_id) do
-      session -> session
-      nil -> nil
+  def is_game_started?(id) do
+    case Registry.lookup(TicTacToe.SessionRegistry, id) do
+      [{pid, _}] -> Process.alive?(pid)
+      [] -> false
     end
   end
 end
