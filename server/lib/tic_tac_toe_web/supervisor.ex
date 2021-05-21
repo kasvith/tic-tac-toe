9ca2daf5ff8df98ca928e7305c4d7391925503ab
Kasun Vithanage <alankasun@gmail.com>
2021-05-21 15:58:23 +0530

    sup

---
+++ server/lib/tic_tac_toe_web/supervisor.ex
@@ -0,0 +1,19 @@
+defmodule TicTacToe.WebSupervisor do
+  use Supervisor
+
+  def start_link(opts \\ []) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end
+
+  @impl true
+  def init(:ok) do
+    children = [
+      {Registry, keys: :unique, name: TicTacToe.SessionRegistry},
+      {Registry, keys: :unique, name: TicTacToe.PlayerRegistry},
+      {Registry, keys: :duplicate, name: TicTacToe.PubSub},
+      TicTacToe.SessionSupervisor
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
