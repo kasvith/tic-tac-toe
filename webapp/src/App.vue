e0ca9786d9dd488b4e425efce323a9295efe841b
Kasun Vithanage <alankasun@gmail.com>
2021-07-06 00:01:54 +0530

    init tailwind

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,22 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="grid grid-cols-3 gap-4 bg-red-900 text-center p-5 m-5 w-96 h-96 ">
+  <div class="border-black">1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+</div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
