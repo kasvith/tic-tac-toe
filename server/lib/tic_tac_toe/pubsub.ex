63c9b786ba121433a011f96bc9c4d738ffaa7a95
Kasun Vithanage <alankasun@gmail.com>
2021-07-28 23:01:12 +0530

    broadcast from

--- server/lib/tic_tac_toe/pubsub.ex
+++ server/lib/tic_tac_toe/pubsub.ex
@@ -1,10 +1,20 @@
 defmodule TicTacToe.PubSub do
   @registry TicTacToe.PubSubRegistry
 
-  def broadcast(topic, message, _current_pid \\ nil) do
+  def broadcast(topic, message) do
     Registry.dispatch(@registry, topic, fn entries ->
       for {pid, _} <- entries, do: send(pid, {:broadcast, message})
     end)
+  end
+
+  def broadcast_from(sender_pid, topic, message) do
+    Registry.dispatch(
+      @registry,
+      topic,
+      fn entries ->
+        for {pid, _} <- entries, pid != sender_pid, do: send(pid, {:broadcast, message})
+      end
+    )
   end
 
   def subscribe(topic) do
